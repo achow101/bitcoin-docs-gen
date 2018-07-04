515d6cdd7178afe907a90be4b3197ecbf58e0722
Andrew Chow <achow101-github@achow101.com>
2018-07-04 10:03:20 +0000

    Update docs

--- docs/search/enumvalues_d.js
+++ docs/search/enumvalues_d.js
@@ -6,7 +6,7 @@ var searchData=
   ['net_5fipv4',['NET_IPV4',['../netaddress_8h.html#acc9a38c714afe79b5035cb36f560dac3ae1598b317168b96919bc3131ee149ebb',1,'netaddress.h']]],
   ['net_5fipv6',['NET_IPV6',['../netaddress_8h.html#acc9a38c714afe79b5035cb36f560dac3a9a9d9355a4923b33382f9894ba80688d',1,'netaddress.h']]],
   ['net_5fmax',['NET_MAX',['../netaddress_8h.html#acc9a38c714afe79b5035cb36f560dac3a675b0737d6c404a5de25e324c166a4b5',1,'netaddress.h']]],
-  ['net_5ftor',['NET_TOR',['../netaddress_8h.html#acc9a38c714afe79b5035cb36f560dac3a9defff41db522fc865d58fc4e7646e9f',1,'netaddress.h']]],
+  ['net_5fonion',['NET_ONION',['../netaddress_8h.html#acc9a38c714afe79b5035cb36f560dac3a74254f483ed8f948cd89e53ce36f484b',1,'netaddress.h']]],
   ['net_5funroutable',['NET_UNROUTABLE',['../netaddress_8h.html#acc9a38c714afe79b5035cb36f560dac3af66f60a80e2a5d99813c3f322cf8a6e1',1,'netaddress.h']]],
   ['netnodeid',['NetNodeId',['../class_peer_table_model.html#a10a32ec5f31c9765c80b611760f7e590a8ee229da5c6b0e98788b9baae0362aa2',1,'PeerTableModel']]],
   ['netunreachable',['NETUNREACHABLE',['../netbase_8cpp.html#a97f842edf4ac3c020601432e9418c02ea52f4169fd4ab94294587e44d04879827',1,'netbase.cpp']]],
